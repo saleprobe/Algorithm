f5808c4e0ca565119ccf97290e08da5dfd343537
홍성민 <77483141+saleprobe@users.noreply.github.com>
2023-02-04 21:13:48 +0900

    subtraction_of_two_numbers
    프로그래머스 두 수의 차

---
+++ finding_the_remainder.py
@@ -0,0 +1,9 @@
+def solution(num1, num2):
+    answer = -1
+    answer = num1 % num2
+    return answer
+
+# 1
+solution(3, 2)
+# 0
+solution(10, 5)
---
+++ subtraction_of_two_numbers.py
@@ -0,0 +1,9 @@
+def solution(num1, num2):
+    answer = 0
+    answer = num1 - num2
+    return answer
+
+# -1
+solution(2, 3)
+# 98
+solution(100, 2)

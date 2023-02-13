6744a9ea5852f3bc0636bffee74e4cd21eb6c196
홍성민 <77483141+saleprobe@users.noreply.github.com>
2023-02-13 23:00:12 +0900

    number_of_ordered_pairs
    프로그래머스 순서쌍의 개수

---
+++ Programmers/number_of_ordered_pairs.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    for num in range(1, n+1):
+        if n % num == 0:
+            answer += 1
+    return answer
+
+# 6
+print(solution(20))
+# 9
+print(solution(100))

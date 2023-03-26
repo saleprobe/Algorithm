6a1d30dc8317e3ad83a28649791e91040c6de3e8
홍성민 <77483141+saleprobe@users.noreply.github.com>
2023-03-26 23:54:53 +0900

    to_calculate_a_string
    프로그래머스 문자열 계산하기

---
+++ Programmers/to_calculate_a_string.py
@@ -0,0 +1,13 @@
+# 문자열 계산하기
+def solution(my_string):
+    s = my_string.split()
+    answer = int(s[0])
+    for i in range(1,len(s),2):
+        if s[i]=='+':
+            answer+=int(s[i+1])
+        else:
+            answer-=int(s[i+1])
+    return answer
+
+# 7
+print(solution("3 + 4"))
